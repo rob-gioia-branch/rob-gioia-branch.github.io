2807cbd107b989c35ecfee1831d6904a21207782
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-05 14:13:08 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,10 @@
-async function logJSONData() {
-  const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
+function loadCharacterImages() {
+  loadCharacterImage("620", "marvel1");
+}
+
+async function loadCharacterImage(characterID, elementID) {
+  const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/" + characterID + ".json");
   const jsonData = await response.json();
-  console.log(jsonData);
+  const image = json.character.images["lg"];
+  document.getElementById(element).src=image;
 }
