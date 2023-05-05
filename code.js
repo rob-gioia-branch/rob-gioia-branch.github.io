88afa182eda9ce7c84a5ab341469092fb1eca420
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-05 15:16:50 -0400

    Update code.js

--- code.js
+++ code.js
@@ -6,5 +6,5 @@ async function loadCharacterImage(characterID, elementID) {
   const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/" + characterID + ".json");
   const jsonData = await response.json();
   const image = jsonData.images["lg"];
-  document.getElementById(element).src=image;
+  document.getElementById(elementID).src=image;
 }
