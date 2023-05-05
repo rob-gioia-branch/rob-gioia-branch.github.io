f70bfe67f121089c2873e8fadffb7577e951c63a
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-05 13:48:41 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
+async function logJSONData() {
+  const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
 
+logJSONData();
