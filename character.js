2120f6ab835f3a37694035969067f15bd75e4750
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-08 14:54:00 -0400

    Update character.js
    Added a character class

--- character.js
+++ character.js
@@ -1,1 +1,10 @@
-
+export class Character {
+    let name;
+    let image;
+    let qrCodeImage;
+    constructor(json) {
+        name = json.name;
+        image = json.images["lg"];
+        qrCodeImage = json.images["sm"];
+    }
+}
