b56ebac9a8950a81723d6c97ead3ca75b588fdba
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-08 14:58:36 -0400

    Update character.js
    Added in some getter functions to retrieve the values of character properties like name, image, and QR code image

--- character.js
+++ character.js
@@ -2,9 +2,22 @@ export class Character {
     let name;
     let image;
     let qrCodeImage;
+    
     constructor(json) {
         name = json.name;
         image = json.images["lg"];
         qrCodeImage = json.images["sm"];
+    }
+    
+    export function getCharacterName() {
+        return name;
+    }
+    
+    export function getCharacterImage() {
+        return image;
+    }
+    
+    export function getQRCodeImage() {
+        return qrCodeImage;
     }
 }
