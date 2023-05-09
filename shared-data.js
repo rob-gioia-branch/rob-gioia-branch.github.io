11a508a638252d160946237aab2d8c93cf2d8dce
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-09 16:20:30 -0400

    Delete shared-data.js

--- shared-data.js
+++
@@ -1,9 +0,0 @@
-let currentCharacter;
-
-export function setCurrentCharacter(character) {
-  currentCharacter = character;
-}
-
-export function getCurrentCharacter() {
-  return currentCharacter;
-}
