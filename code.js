8fd4b5b6c7bca757125307eff414755181f06baa
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-09 09:50:07 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-//import * as sharedData from './shared-data.js';
+import * as sharedData from './shared-data.js';
 
 var marvelCharacterIds = ["620", "332", "149", "659", "717", "423", "196", "374"]; 
 var marvelElementIds = ["marvel1", "marvel2", "marvel3", "marvel4", "marvel5", "marvel6", "marvel7", "marvel8"];
@@ -35,8 +35,8 @@ async function loadCharacterImages() {
 async function loadCharacterImage(characterID, elementID) {
   const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/" + characterID + ".json");
   const jsonData = await response.json();
-  //const character = new Character(jsonData);
-  //sharedData.setCurrentCharacter(character);
+  const character = new Character(jsonData);
+  sharedData.setCurrentCharacter(character);
   const image = jsonData.images["lg"];
   const qrCodeImage = jsonData.images["sm"];
   const name = jsonData.name;
