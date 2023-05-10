68f3d9876751a7003befab4c46900e5e2a5c1def
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-10 10:44:36 -0400

    Update character.js

--- character.js
+++ character.js
@@ -2,7 +2,7 @@ export class Character {
     constructor(json) {
         this.name = json.name;
         this.image = json.images["lg"];
-        this.qrCodeImage = json.images["sm"];
+        this.qrCodeImage = json.images["xs"];
     }
     
     getCharacterName() {
