83833b422d5880da260236e1aee739ba06334869
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-10 11:11:57 -0400

    Update branch-utils.js

--- branch-utils.js
+++ branch-utils.js
@@ -45,7 +45,7 @@ function createQRCode() {
     "code_color":"#000000",
     "background_color": "#FFFFFF",
     "margin": 5,
-    "width": 256,
+    "width": 300,
     "image_format": "png",
     "center_logo_url": qrCodeImage
   };
