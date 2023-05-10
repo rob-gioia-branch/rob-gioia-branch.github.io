0e5e681f6a6ce45da0237e24a60d078f76377bd1
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-10 14:41:36 -0400

    Update branch-utils.js

--- branch-utils.js
+++ branch-utils.js
@@ -11,11 +11,11 @@ function initializeBranch(isTestMode = false) {
   (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking qrCode".split(" "), 0);
   //initialize Branch with the live key or test
   if(isTestMode) {
-    branch.init('key_test_hcGYfaAnBPHUutc7SRmrSgjdCrgZ30RL'), function(err, data) {
+    branch.init('key_test_hcGYfaAnBPHUutc7SRmrSgjdCrgZ30RL', function(err, data) {
       console.log(err, data);
   });
   } else {
-    branch.init('key_live_ccQ8piFdCMPVysh8TLmEhghmuCk162Rr'), function(err, data) {
+    branch.init('key_live_ccQ8piFdCMPVysh8TLmEhghmuCk162Rr', function(err, data) {
       console.log(err, data);
   });
   }
