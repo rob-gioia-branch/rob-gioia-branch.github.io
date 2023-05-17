9cdde294e78ca6e7b74be99459293619ad1cfac0
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-17 11:31:31 -0400

    Update branch-utils.js

--- branch-utils.js
+++ branch-utils.js
@@ -1,4 +1,5 @@
 var isTrackingDisabled = false;  
+var isFirstLoad = true;
 
 /*
   Load and initialize the Branch SDK
@@ -7,6 +8,8 @@ var isTrackingDisabled = false;
    • If you'd like to use a specific version of the SDK, point to https://cdn.branch.io/branch-x.xx.x.min.js (e.g. https://cdn.branch.io/branch-2.47.1.min.js) rather than https://cdn.branch.io/branch-latest.min.js when initializing
 */
 function initializeBranch(isTestMode = false) {
+  if(!isFirstLoad) { return; }
+  isFirstLoad = false;
   //load the Branch SDK
   (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking qrCode".split(" "), 0);
   //initialize Branch with the live key or test
