e9da0cb92f9dffacdc3736a9ae7fcecb07191419
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-17 14:29:27 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
-export function loadCharacterDetailView(character) {
+export function loadCharacterDetailView() {
     const listView = document.getElementById("list-view");
     const detailView = document.getElementById("detail-view");
     listView.hidden = true;
     detailView.hidden = false;
     const characterName = document.getElementById("character-name");
     const characterImage = document.getElementById("character-image");
-    characterName.innerHTML = character.getCharacterName();
-    characterImage.src = character.getCharacterImage();
+    characterName.innerHTML = window.character.getCharacterName();
+    characterImage.src = window.character.getCharacterImage();
 }
