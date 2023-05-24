b0b648ad47ea3fe33956258a98e703c9d36e0591
rob-gioia-branch <87081315+rob-gioia-branch@users.noreply.github.com>
2023-05-24 15:08:02 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var marvelCharacterIds = ["620", "332", "149", "659", "717", "423", "196", "374"]; 
+var marvelCharacterIds = ["311", "312", "313", "314", "315", "423", "196", "374"]; 
 var marvelElementIds = ["marvel1", "marvel2", "marvel3", "marvel4", "marvel5", "marvel6", "marvel7", "marvel8"];
 
 var dcCharacterIds = ["70", "644", "298", "266", "370", "514", "558", "63"];
